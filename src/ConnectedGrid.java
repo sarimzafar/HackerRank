9cd94f37dfff7c8d0f361e9fa5064df8afaf48df
Sarim Zafar <shzafar@uwaterloo.ca>
2015-05-28 09:49:21 -0400

    Updated Problem Statement

--- src/ConnectedGrid.java
+++ src/ConnectedGrid.java
@@ -3,7 +3,44 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 import java.util.Scanner;
 
 /**
- * Created by shassanzafar on 5/20/2015.
+ *Problem Statement
+
+You are given a matrix with m rows and n columns of cells, each of which contains either 1 or 0. Two cells are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally. The connected and filled (i.e. cells that contain a 1) cells form a region. There may be several regions in the matrix. Find the number of cells in the largest region in the matrix.
+
+Input Format
+There will be three parts of t input:
+The first line will contain m, the number of rows in the matrix.
+The second line will contain n, the number of columns in the matrix.
+This will be followed by the matrix grid: the list of numbers that make up the matrix.
+
+Output Format
+Print the length of the largest region in the given matrix.
+
+Constraints
+0<m<10
+0<n<10
+
+Sample Input:
+
+4
+4
+1 1 0 0
+0 1 1 0
+0 0 1 0
+1 0 0 0
+Sample Output:
+
+5
+Task: 
+Write the complete program to find the number of cells in the largest region.
+
+Explanation
+
+X X 0 0
+0 X X 0
+0 0 X 0
+1 0 0 0  
+The X characters indicate the largest connected component, as per the given definition. There are five cells in this component.
  */
 public class ConnectedGrid {
     public int row;
